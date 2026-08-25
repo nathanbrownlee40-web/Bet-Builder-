55ccd26d72dc1b3d410187a0277f435dd219cb10
nathanbrownlee40-web <nathanbrownlee40@gmail.com>
2026-08-25 22:50:01 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='top-daily-builders-recovered-v2';
+const CACHE='top-daily-builders-recovered-v3';
 const APP_SHELL=['./','./index.html','./manifest.json','./icon-192x192.png','./icon-512x512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>Promise.all(APP_SHELL.map(u=>fetch(u,{cache:'no-store'}).then(r=>r.ok?c.put(u,r):null).catch(()=>null)))).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
